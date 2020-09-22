ea5efa61eb8a928fa5b5a7dda0d422b5ebb93b05
Zhexin Jin (FA Talent) <v-zhexji@microsoft.com>
2020-09-22 10:10:20 +0800

    postprocess

--- src/Microsoft.Health.Fhir.Anonymizer.Shared.Core/Utility/TextRecognizer.cs
+++ src/Microsoft.Health.Fhir.Anonymizer.Shared.Core/Utility/TextRecognizer.cs
@@ -1,11 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using System.Text.RegularExpressions;
 using Microsoft.Health.Fhir.Anonymizer.Core.Models;
 using Microsoft.Recognizers.Text;
 using Microsoft.Recognizers.Text.DateTime;
-using Microsoft.Recognizers.Text.Number;
 using Microsoft.Recognizers.Text.NumberWithUnit;
 using Microsoft.Recognizers.Text.Sequence;
 
@@ -13,10 +12,13 @@ namespace Microsoft.Health.Fhir.Anonymizer.Core.Utility
 {
     public class TextRecognizer : INamedEntityRecognizer
     {
+        public string[] IdRegex = { @"MRN", @"code", @"id" };
+        public int WindowBefore = 10;
+        public int WindowAfter = 0;
+
         public List<Entity> RecognizeText(string text)
         {
             var culture = Culture.English;
-            var recognitionResults = new List<Entity>();
 
             var modelResults = new List<ModelResult>();
             modelResults.AddRange(NumberWithUnitRecognizer.RecognizeAge(text, culture));
@@ -27,6 +29,7 @@ namespace Microsoft.Health.Fhir.Anonymizer.Core.Utility
             modelResults.AddRange(SequenceRecognizer.RecognizeGUID(text, culture));
             modelResults.AddRange(SequenceRecognizer.RecognizeURL(text, culture));
 
+            var recognitionResults = new List<Entity>();
             foreach (var modelResult in modelResults)
             {
                 recognitionResults.Add(new Entity() 
@@ -39,14 +42,28 @@ namespace Microsoft.Health.Fhir.Anonymizer.Core.Utility
                 });
             }
 
-            recognitionResults = EntityProcessUtility.ProcessEntities(recognitionResults);
+            recognitionResults = Postprocess(text, recognitionResults);
 
-            // foreach (var entity in recognitionResults)
-            // {
-            //     Console.WriteLine($"{entity.Text} {entity.Category} {entity.Offset} {entity.Length}");
-            // }
+            recognitionResults = EntityProcessUtility.ProcessEntities(recognitionResults);
             
             return recognitionResults;
+        }
+
+        public List<Entity> Postprocess(string text, List<Entity> entities)
+        {
+            foreach (var entity in entities)
+            {   
+                if (entity.Category == "phonenumber")
+                {
+                    var startIndex = Math.Max(0, entity.Offset - WindowBefore);
+                    var endIndex = Math.Min(text.Length, entity.Offset + entity.Length + WindowAfter);
+                    if (IdRegex.Any(regex => Regex.IsMatch(text.Substring(startIndex, endIndex - startIndex), regex, RegexOptions.IgnoreCase)))
+                    {
+                        entity.Category = "id";
+                    }
+                }
+            }
+            return entities;
         }
     }
 }
